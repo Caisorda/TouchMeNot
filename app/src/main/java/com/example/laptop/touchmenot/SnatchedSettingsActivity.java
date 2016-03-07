09c487c1a336087706e2823e029a45d98d33c1ba
Caisorda <juan_salceda@dlsu.edu.ph>
2016-03-08 00:22:50 +0800

    Button Detection Setting layout
    just the gui

---
+++ app/src/main/java/com/example/laptop/touchmenot/SnatchedSettingsActivity.java
@@ -0,0 +1,14 @@
+package com.example.laptop.touchmenot;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SnatchedSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.snatched_settings);
+    }
+}
