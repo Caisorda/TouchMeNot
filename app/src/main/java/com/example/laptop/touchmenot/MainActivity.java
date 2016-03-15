6788ff514111b908f6fa8208a666d68ddfa9ecf0
Bryan Coquilla <coquilla0906@gmail.com>
2016-03-16 01:24:56 +0800

    Update

--- app/src/main/java/com/example/laptop/touchmenot/MainActivity.java
+++ app/src/main/java/com/example/laptop/touchmenot/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.laptop.touchmenot;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     RelativeLayout rlCharger, rlMotion, rlSim, rlSettings;
+    boolean active = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +36,45 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        rlCharger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BroadcastReceiver receiver = new BroadcastReceiver() {
+                    public void onReceive(Context context, Intent intent) {
+                        int state = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+                        if ((state == BatteryManager.BATTERY_PLUGGED_AC
+                                || state == BatteryManager.BATTERY_PLUGGED_USB) && active == false) {
+                            Toast.makeText(getApplicationContext(), "Charging and now activating",
+                                    Toast.LENGTH_SHORT).show();
+                            active = true;
+                        }
+
+                        else if ((state == BatteryManager.BATTERY_PLUGGED_AC
+                                || state == BatteryManager.BATTERY_PLUGGED_USB) && active == true) {
+                            Toast.makeText(getApplicationContext(), "Charging and now deactivating",
+                                    Toast.LENGTH_SHORT).show();
+                            active = false;
+                        }
+
+                        else if (state == 0) {
+                            // not charging or on battery power
+                            Toast.makeText(getApplicationContext(), "Not charging.",
+                                    Toast.LENGTH_SHORT).show();
+                            active = false;
+                        }
+                    }
+                };
+                IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+                registerReceiver(receiver, filter);
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
 
     }
 }
